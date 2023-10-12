301eaf19a9a3958650522ec86749e59e6c26a306
SamaadTurner <samaad.turner@gmail.com>
2023-10-11 19:50:57 -0700

    completed lab

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# image-lambda
+# Project: AWS - S3 and Lambda
+
+## Author: Samaad Turner
+
+## Problem Domain
+
+Create an S3 Bucket with “open” read permissions, so that anyone can see the images/files in their browser. A user should be able to upload an image at any size, and update a dictionary of all images that have been uploaded so far.  
+
+## Links and Resources  
+
+
+
+## How To Use Lambda
+
+ When you upload an image with a .png extension, it will trigger the lambda.
+
--- index.js
+++ index.js
@@ -1,55 +1,68 @@
-const { S3 } = require('@aws-sdk/client-s3');
+import { S3 } from '@aws-sdk/client-s3'; // code preinstalled by AWS, to do S3 operations -> Reading and Writing
 
 let s3client = new S3({ region: 'us-west-2' });
 
-exports.handler = async (event) => {
+// this is not banana, your lambda must export a function named "handler"
+export const handler = async (event) => {
+
+  let fileExtensionRegex = new RegExp('\.[a-zA-Z0-9]+$');
+
   console.log('Lambda code is running!');
   const bucketName = event.Records[0].s3.bucket.name;
   const fileName = event.Records[0].s3.object.key;
+  const fileSize = event.Records[0].s3.object.size;
+  const fileExtension = event.Records[0].s3.object.key.match(fileExtensionRegex)[0];
 
-  // Fetch existing images.json from S3, or initialize an empty array if it doesn't exist.
-  let images = [];
+  const imageInfo = {
+    name: fileName,
+    size: fileSize,
+    type: fileExtension,
+  };
+
+  console.log('OUR NEWLY ADDED FILE', imageInfo);
+
+  // read command using the sdk
+  const command = {
+    Bucket: bucketName,
+    Key: 'images.json',
+  };
+
+  let imagesArray = null;
+
   try {
-    const data = await s3client.getObject({ Bucket: bucketName, Key: 'images.json' });
-    images = JSON.parse(data.Body.toString());
-  } catch (err) {
-    if (err.code !== 'NoSuchKey') {
-      console.error('Error fetching images.json:', err);
-      throw err;
+    let result = await s3client.getObject(command);
+    // result is an array here
+
+    let stringifiedArray = await result.Body.transformToString();
+
+    //strinigifiedArray.json(); // parse the string as json
+    imagesArray = JSON.parse(stringifiedArray);
+    imagesArray.push(imageInfo);
+
+  } catch (e) {
+    console.log('ERROR OCCURED WHEN READING image.json FILE', e);
+    if (e.Code === 'NoSuchKey') {
+      // create a new Array, save to images.json
+      imagesArray = [];
+      imagesArray.push(imageInfo);
     }
   }
 
-  // If the uploaded file is not images.json, then it's an image.
-  // Update the images array with the metadata of the new image.
-  if (fileName !== 'images.json') {
-    const imageMetadata = {
-      Name: fileName,
-      Size: event.Records[0].s3.object.size,
-      Type: fileName.split('.').pop(),
-    };
+  console.log('OUR IMAGES ARRAY: ', imagesArray);
 
-    // If the image with the same name exists, update it, else add a new entry.
-    const existingImageIndex = images.findIndex(img => img.Name === fileName);
-    if (existingImageIndex > -1) {
-      images[existingImageIndex] = imageMetadata;
-    } else {
-      images.push(imageMetadata);
-    }
+  command.Body = JSON.stringify(imagesArray);
 
-    // Re-upload the updated images.json back to S3
-    await s3client.putObject({
-      Bucket: bucketName,
-      Key: 'images.json',
-      Body: JSON.stringify(images),
-      ContentType: 'application/json',
-    });
+  try {
+    let result = await s3client.putObject(command);
+    console.log(result);
+  } catch (e) {
+    console.log(e);
   }
 
-  // Return a response. Note: This is more useful if your Lambda is being used as an HTTP endpoint.
-  // For an S3 trigger, the response won't be seen directly by end-users.
+  // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Processed ' + fileName),
-  };
+    body: JSON.stringify('Added ' + fileName + 'to the manifest'),
+  }; // lambdas use some of the same practices that HTTP servers.
   return response;
 };
